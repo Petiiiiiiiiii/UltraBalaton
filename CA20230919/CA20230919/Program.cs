b0fe9899b7059f2b93b3405564a10e0e806beb10
Orosz Péter <70895314+Petiiiiiiiiii@users.noreply.github.com>
2023-09-19 13:11:40 +0200

    Add files via upload

---
+++ CA20230919/CA20230919/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+namespace CA20230919
+{
+    class Program
+    {
+        static void Main()
+        {
+            var versenyzok = new List<Versenyzo>();
+
+            using var sr = new StreamReader(
+                path: @"..\..\..\src\ub2017egyeni.txt",
+                encoding: Encoding.UTF8);
+
+            _ = sr.ReadLine();
+
+            while (!sr.EndOfStream)
+                versenyzok.Add(new Versenyzo(sr.ReadLine()));
+
+            Console.WriteLine($"3.feladat: egyeni indulók száma: {versenyzok.Count}");
+
+            var f4 = versenyzok.Count(v => !v.Kategoria && v.Tavszazalek == 100);
+            Console.WriteLine($"4. feladat: teljes tavot teljesítő nők száma: {f4} fő");
+
+            Console.Write("5. feladat: Kérem a sportoló nevét: ");
+            string nev = Console.ReadLine().ToLower();
+
+            Versenyzo f5 = versenyzok.FirstOrDefault(v => v.Nev.ToLower() == nev);
+            if (f5 != null)
+                Console.WriteLine("\tIndult egyeniben a sportolo? NEM");
+            else
+                Console.WriteLine("\tIndult egyeniben a sportolo? IGEN");
+                Console.WriteLine($"\tTeljesitette a telejes távot: " +
+                    $"{(f5.Tavszazalek == 100 ? "IGEN" : "NEM")}");
+
+            var f7 = versenyzok
+                .Where(v => v.Kategoria && v.Tavszazalek == 100)
+                .Average(v => v.IdoOraban);
+
+            Console.WriteLine($"7. feladat: férfiak átlagideje: {f7:0.00} óra");
+
+            var f8n = versenyzok
+                .Where(v => !v.Kategoria && v.Tavszazalek == 100)
+                .OrderByDescending(v => v.Versenyido)
+                .First();
+
+            var f8f = versenyzok
+                .Where(v => v.Kategoria && v.Tavszazalek == 100)
+                .OrderByDescending(v => v.Versenyido)
+                .First();
+
+            Console.WriteLine("8.feladat verseny győztesei:");
+            Console.WriteLine($"Nők : {f8n.Nev}");
+            Console.WriteLine($"Férfiak : {f8f.Nev}");
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CA20230919/CA20230919/Uszo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CA20230919
+{
+    class Versenyzo
+    {
+        public string Nev { get; set; }
+        public string Rajtszam { get; set; }
+        public bool Kategoria { get; set; }
+        public TimeSpan Versenyido { get; set; }
+        public int Tavszazalek { get; set; }
+
+        public double IdoOraban => Versenyido.TotalHours;
+
+        public Versenyzo(string sor)
+        {
+            var v = sor.Split(';');
+            Nev = v[0];
+            Rajtszam = v[1];
+            Kategoria = v[2] == "Ferfi";
+            var ido = v[3].Split(':');
+            Versenyido = new TimeSpan(
+                hours: int.Parse(ido[0]),
+                minutes: int.Parse(ido[1]),
+                seconds: int.Parse(ido[2])
+                );
+            Tavszazalek = int.Parse(v[4]);
+        }
+    }
+}
